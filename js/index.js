b6ff800ac6f8be247c4949ceea8e0a98f26078a1
Sheptus <sergey.sheptus@gmail.com>
2019-07-19 03:53:46 +0300

    adding bootstrap npm folder + css links

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//* Task Notes Board App *\\
+
+const NOTES_DOM = [{
+    note_date: document.getElementById("noteDate"),
+    note_time: document.getElementById("noteTime"),
+    note_desc: document.getElementById("moteDesc"),
+    create_notes_data: document.getElementById("notesData"),
+    note_date: document.getElementById(""),
+}]
