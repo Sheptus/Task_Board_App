d8282bfa134504bea03439f6fc5816b62243bfab
Sheptus <sergey.sheptus@gmail.com>
2019-07-20 12:46:43 +0300

    plug-ins update

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const NOTES_DOM = [{
     note_date: document.getElementById("noteDate"),
     note_time: document.getElementById("noteTime"),
-    note_desc: document.getElementById("moteDesc"),
-    create_notes_data: document.getElementById("notesData"),
+    note_desc: document.getElementById("noteDesc"),
+    create_notes_form: document.getElementById("notesData"),
     note_date: document.getElementById(""),
 }]
